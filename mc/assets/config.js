dca82d2f407130093f42c0a5e5c8403ccaf327d1
mag1182 <mag1182@rocketmail.com>
2020-02-11 19:22:45 -0600

    Add files via upload

---
+++ mc/assets/config.js
@@ -0,0 +1,50 @@
+// cpanel - site_templates/vcard_dev/assets/config.js.tt Copyright(c) 2016 cPanel, Inc.
+//                                                          All rights Reserved.
+// copyright@cpanel.net                                        http://cpanel.net
+// This code is subject to the cPanel license. Unauthorized copying is prohibited
+
+window.cpanel = {
+    data: {
+        phone: "(615)-775-2751",
+        fax: "",
+        email: "cochack1182@gmail.com",
+        address: "Lebanon TN",
+        social: [
+            
+            {
+                icon: 'facebook',
+                link: "https:\/\/www.facebook.com\/sebastian.robinson.3994"
+            },
+            
+            
+            
+            {
+                icon: 'instagram',
+                link: "https:\/\/www.instagram.com\/mag1182\/"
+            },
+            
+            
+            
+            
+        ]
+    },
+    style: {
+        primary: "",
+        accent: ""
+    },
+    slides: [
+        {
+            prefix: "Hello My name is",
+            title: "Sebastian Robinson",
+            subtitle: "Game\/Website Developer",
+            type: 'vcard',
+            backgroundImage: "",
+            backgroundColor: "",
+            color: "",
+            buttonText: "Visit Website",
+            buttonLink: "http:\/\/mag1182.com\/home.html",
+            biography: "",
+            portraitImage: ""
+        }
+    ]
+};
